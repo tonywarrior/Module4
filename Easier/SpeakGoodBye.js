b65c5ab83a512b7c2a54a423b16d19bc51f9c048
tonywarrior <antonio.varela1986@gmail.com>
2017-03-12 23:06:46 +0100

    add

--- Easier/SpeakGoodBye.js
+++ Easier/SpeakGoodBye.js
@@ -4,6 +4,17 @@
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
+(function (window) {
+  var byeSpeaker = {};
+  byeSpeaker.name = "John", "Jen","Jason","Jim";
+  var speakWord = "Goodbye ";
+  byeSpeaker.speak = function () {
+    console.log(speakWord + "John", "Jen","Jason","Jim" + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
@@ -11,14 +22,14 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+//var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+//function speak(name) {
+//  console.log(speakWord + " " + name);
+//}
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
